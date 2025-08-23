c86882378ca8f4bb4ebb309f9fa34f6db0d5ab86
Arctan <175182882+aarctanz@users.noreply.github.com>
2025-08-23 20:53:49 +0530

    custom line parsing, int64 instead of float

--- main.go
+++ main.go
@@ -5,21 +5,55 @@ import (
 	"fmt"
 	"os"
 	"sort"
-	"strconv"
 	"strings"
 )
 
 type Station struct {
 	Name  string
-	Max   float64
-	Min   float64
-	Sum   float64
+	Max   int64
+	Min   int64
+	Sum   int64
 	Count int
+}
+
+func parseLines(line string) (string, int64) {
+	
+	i := strings.Index(line, ";")
+	name := line[:i]
+
+	chunk := line[i+1:]
+	neg := false
+
+	if chunk[0] == '-' {
+		neg = true
+	}
+
+	var temp int64
+
+	for i := 0; i < len(chunk); i++ {
+		if chunk[i] == '\n' {
+			break
+		}
+		if chunk[i]=='-' {
+			continue
+		}
+		
+		if chunk[i]=='.' {
+			continue
+		}
+
+		temp = temp*10 + int64(chunk[i]-'0')
+	}
+	if neg {
+		temp = -temp
+	}
+	return name, temp
+
 }
 
 func main() {
 	stations := make(map[string]*Station)
-	f, err := os.Open("./data/mm.txt")
+	f, err := os.Open("./data/measurements.txt")
 	if err != nil {
 		panic(err)
 	}
@@ -28,30 +62,25 @@ func main() {
 	scanner := bufio.NewScanner(f)
 
 	for scanner.Scan() {
-		lines := scanner.Text()
-		parts := strings.Split(lines, ";")
-
-		name := parts[0]
-		value, err := strconv.ParseFloat(parts[1], 64)
-		if err != nil {
-			panic(err)
-		}
+		line := scanner.Text()
+		
+		name, temp := parseLines(line)
 
 		if _, ok := stations[name]; !ok {
 			stations[name] = &Station{
 				Name:  name,
-				Max:   value,
-				Min:   value,
-				Sum:   value,
+				Max:   temp,
+				Min:   temp,
+				Sum:   temp,
 				Count: 1,
 			}
 		} else {
-			if value > stations[name].Max {
-				stations[name].Max = value
-			} else if value < stations[name].Min {
-				stations[name].Min = value
+			if temp > stations[name].Max {
+				stations[name].Max = temp
+			} else if temp < stations[name].Min {
+				stations[name].Min = temp
 			}
-			stations[name].Sum += value
+			stations[name].Sum += temp
 			stations[name].Count++
 		}
 
@@ -66,7 +95,6 @@ func main() {
 	for station := range stations {
 		keys = append(keys, station)
 	}
-
 	sort.Strings(keys)
 
 	fmt.Fprint(os.Stdout, "{")
@@ -75,8 +103,8 @@ func main() {
 			fmt.Fprint(os.Stdout, ", ")
 		}
 		s := stations[station]
-		mean := s.Sum / float64(s.Count)
-		fmt.Fprintf(os.Stdout, "%s=%.1f/%.1f/%.1f", station, s.Min, mean, s.Max)
+		mean := s.Sum / int64(s.Count)
+		fmt.Fprintf(os.Stdout, "%s=%.1f/%.1f/%.1f", station, (float64)(s.Min/10), (float64)(mean/10), (float64)(s.Max/10))
 	}
 	fmt.Fprint(os.Stdout, "}\n")
 }
